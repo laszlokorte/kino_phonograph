99a17037972a05e4546f99b9574ae22942d6972f
Laszlo Korte <email@laszlokorte.de>
2026-04-20 15:09:51 +0200

    fix vectorization and release new version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule KinoPhonograph.MixProject do
   def project do
     [
       app: :kino_phonograph,
-      version: "0.6.0",
+      version: "0.7.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
